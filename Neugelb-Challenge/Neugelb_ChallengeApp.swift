c6d7ac3930e45455b8729ce1b84787c6a712cf66
Taranjeet Kaur <taranjeet.kaur@yassir.com>
2026-04-11 16:01:16 +1200

    Initial Commit

---
+++ Neugelb-Challenge/Neugelb_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neugelb_ChallengeApp.swift
+//  Neugelb-Challenge
+//
+//  Created by Taranjeet Kaur on 11/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Neugelb_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
